0fafa87d054d4350624ed4264b039e86883a3751
Francisco Franco <franciscofranco.1990@gmail.com>
2017-04-21 09:31:43 +0800

    drivers: wakeup: run the wakelock blockers during wakeup_source activ… …ation and every resume
    Clean up the implementation to make it usable in more than one function.
    Run it every resume to make sure the wakeup source isn't still alive and kicking.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -863,6 +863,7 @@ void pm_print_active_wakeup_sources(void)
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
 		if (ws->active) {
 			pr_info("active wakeup source: %s\n", ws->name);
+                active = 1;
 			if (!wakeup_source_blocker(ws))
 				active = 1;
 		} else if (!active &&
