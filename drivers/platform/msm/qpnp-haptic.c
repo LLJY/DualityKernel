2104ecf3c0ab07ffbb723768b5a6e6032c16a0d1
lucas <lucasleeeeeeeee@gmail.com>
2017-04-30 23:26:56 +0800

    qpnp-haptic: Add sysfs interface to control intensity
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -1346,14 +1346,12 @@ static ssize_t qpnp_hap_vmax_mv_store(struct device *dev,
 	if (sscanf(buf, "%d", &data) != 1)
 		return -EINVAL;
 
-	if (data < hap->vtg_min) {
-		pr_err("%s: mv %d not in range (%d - %d), using min.", __func__, data,
-				hap->vtg_min, hap->vtg_max);
-		data = hap->vtg_min;
-	} else if (data > hap->vtg_max) {
-		pr_err("%s: mv %d not in range (%d - %d), using max.", __func__, data,
-				hap->vtg_min, hap->vtg_max);
-		data = hap->vtg_max;
+	if (data < QPNP_HAP_VMAX_MIN_MV) {
+		pr_err("%s: mv %d not in range (%d - %d), using min.", __func__, data, QPNP_HAP_VMAX_MIN_MV, QPNP_HAP_VMAX_MAX_MV);
+		data = QPNP_HAP_VMAX_MIN_MV;
+	} else if (data > QPNP_HAP_VMAX_MAX_MV) {
+		pr_err("%s: mv %d not in range (%d - %d), using max.", __func__, data, QPNP_HAP_VMAX_MIN_MV, QPNP_HAP_VMAX_MAX_MV);
+		data = QPNP_HAP_VMAX_MAX_MV;
 	}
 
 	hap->vmax_mv = data;
@@ -1364,6 +1362,7 @@ static ssize_t qpnp_hap_vmax_mv_store(struct device *dev,
 	return strnlen(buf, count);
 }
 
+<<<<<<< HEAD
 static ssize_t qpnp_hap_min_show(struct device *dev,
 					struct device_attribute *attr,
 					char *buf)
@@ -1397,6 +1396,8 @@ static ssize_t qpnp_hap_default_show(struct device *dev,
 	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_default);
 }
 
+=======
+>>>>>>> d2b984c... qpnp-haptic: Add sysfs interface to control intensity
 /* sysfs attributes */
 static struct device_attribute qpnp_hap_attrs[] = {
 	__ATTR(wf_s0, (S_IRUGO | S_IWUSR | S_IWGRP),
@@ -1444,6 +1445,7 @@ static struct device_attribute qpnp_hap_attrs[] = {
 	__ATTR(min_max_test, (S_IRUGO | S_IWUSR | S_IWGRP),
 			qpnp_hap_min_max_test_data_show,
 			qpnp_hap_min_max_test_data_store),
+<<<<<<< HEAD
 	__ATTR(vtg_level, (S_IRUGO | S_IWUSR | S_IWGRP),
 			qpnp_hap_vmax_mv_show,
 			qpnp_hap_vmax_mv_store),
@@ -1456,6 +1458,11 @@ static struct device_attribute qpnp_hap_attrs[] = {
 	__ATTR(vtg_default, S_IRUGO,
 			qpnp_hap_default_show,
 			NULL),
+=======
+	__ATTR(vmax_mv, (S_IRUGO | S_IWUSR | S_IWGRP),
+			qpnp_hap_vmax_mv_show,
+			qpnp_hap_vmax_mv_store),
+>>>>>>> d2b984c... qpnp-haptic: Add sysfs interface to control intensity
 };
 
 static int calculate_lra_code(struct qpnp_hap *hap)
