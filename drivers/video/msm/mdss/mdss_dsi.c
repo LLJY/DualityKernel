7f0150faa9930d5b7b9c96b6b68bb3e2f64c5dd5
lucas <lucasleeeeeeeee@gmail.com>
2017-05-06 22:00:56 +0800

    (ignore this commit)

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -2553,7 +2553,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		break;
 	case MDSS_EVENT_UNBLANK:
 		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
-		mdss_dsi_get_hw_revision(ctrl_pdata);
 
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
