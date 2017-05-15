21f05821232570833c494e8caaf5142d883f7fe6
Lucas <lucasleeeeeeeee@gmail.com>
2017-05-15 23:52:39 +0800

    Revert "Added Powersuspend"
    This reverts commit 00ccc0f97a44451218d7e9b55f47cd253c5cabb8.

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -2552,8 +2552,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
-		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
-
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		break;
