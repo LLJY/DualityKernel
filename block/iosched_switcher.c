85b67fe598d5c71dcd0f5ca6c77c5b8a25d526c1
LLJY <lucasleeeeeeeee@gmail.com>
2017-07-13 23:54:35 +0800

    iosched switcher: revert to raptor branch

--- block/iosched_switcher.c
+++ block/iosched_switcher.c
@@ -29,7 +29,6 @@ struct req_queue_data {
 };
 
 static struct delayed_work restore_prev;
-static struct delayed_work sleep_sched;
 static DEFINE_SPINLOCK(init_lock);
 static struct req_queue_data req_queues = {
 	.list = LIST_HEAD_INIT(req_queues.list),
@@ -76,11 +75,8 @@ static int fb_notifier_callback(struct notifier_block *nb,
 		 * Switch back from noop to the original iosched after a delay
 		 * when the screen is turned on.
 		 */
-		if (delayed_work_pending(&sleep_sched))
-			cancel_delayed_work_sync(&sleep_sched);
-		queue_delayed_work(system_power_efficient_wq,
-			&restore_prev,
-			msecs_to_jiffies(RESTORE_DELAY_MS));
+		schedule_delayed_work(&restore_prev,
+				msecs_to_jiffies(RESTORE_DELAY_MS));
 		break;
 	default:
 		/*
@@ -88,10 +84,8 @@ static int fb_notifier_callback(struct notifier_block *nb,
 		 * the fb notifier chain call in case weird things can happen
 		 * when switching elevators while the screen is off.
 		 */
-		if (delayed_work_pending(&restore_prev))
-			cancel_delayed_work_sync(&restore_prev);
-		schedule_delayed_work(&sleep_sched,
-				msecs_to_jiffies(RESTORE_DELAY_MS));
+		cancel_delayed_work_sync(&restore_prev);
+		change_all_elevators(&req_queues.list, true);
 	}
 
 	return NOTIFY_OK;
@@ -104,11 +98,6 @@ static struct notifier_block fb_notifier_callback_nb = {
 static void restore_prev_fn(struct work_struct *work)
 {
 	change_all_elevators(&req_queues.list, false);
-}
-
-static void set_sleep_sched_fn(struct work_struct *work)
-{
-	change_all_elevators(&req_queues.list, true);
 }
 
 int init_iosched_switcher(struct request_queue *q)
@@ -131,7 +120,6 @@ int init_iosched_switcher(struct request_queue *q)
 static int iosched_switcher_core_init(void)
 {
 	INIT_DELAYED_WORK(&restore_prev, restore_prev_fn);
-	INIT_DELAYED_WORK(&sleep_sched, set_sleep_sched_fn);
 	fb_register_client(&fb_notifier_callback_nb);
 
 	return 0;
